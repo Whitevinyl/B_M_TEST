72fc292979384ccf39ab5826328dfbacb5948102
Whitevinyl <neverest_songs@yahoo.co.uk>
2017-01-19 18:48:45 +0000

    metronome

--- js/node/audioComponents/core/Clock.js
+++ js/node/audioComponents/core/Clock.js
@@ -6,6 +6,11 @@ var common = require('../common/Common');
 var timeSignature = require('../TimeSignature');
 var marker = require('../Marker');
 
+var sine = require('../voices/Sine');
+
+//-------------------------------------------------------------------------------------------
+//  CLOCK
+//-------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------
 //  INIT
 //-------------------------------------------------------------------------------------------
@@ -37,7 +42,6 @@ proto.setup = function(bpm,beats,division) {
     this.signature.division = division || this.signature.division;
 
     var beatLength = Math.floor((sampleRate * 60) / this.bpm);
-    this.measureIndex = 0;
     this.measureDuration = beatLength * this.signature.beats;
 
 
@@ -57,23 +61,85 @@ proto.setup = function(bpm,beats,division) {
 
 proto.process = function(signal,index) {
 
+    var l,i;
+
     // update the origin of each measure //
+    // place new metronome click markers //
     if (index >= this.measureIndex + this.measureDuration) {
         this.measureIndex = index;
+        this.setup(this.bpm,this.signature.beats,this.signature.division);
     }
 
 
     // create clicks //
-    
+    l = this.markers.length;
+    for (i=0; i<l; i++) {
+        var marker = this.markers[i];
+        if (index === (this.measureIndex + marker.time)) {
+            this.clicks.push( new MetroClick(this.adsr,marker.pitch));
+        }
+    }
 
 
     // process any active clicks //
-    var l = this.clicks.length;
-    for (var i=0; i<l; i++) {
+    l = this.clicks.length;
+    for (i=0; i<l; i++) {
         signal = this.clicks.process(signal,index);
     }
 
     return signal;
+};
+
+
+
+
+//-------------------------------------------------------------------------------------------
+//  METRONOME CLICK
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+//  INIT
+//-------------------------------------------------------------------------------------------
+
+function MetroClick(adsr,pitch) {
+    this.a = 0;
+    this.adsr = adsr || [0,10,1,90];
+    this.pitch = pitch || 220;
+    this.voice = new sine();
+    this.duration = 5000;
+    this.i = 0;
+}
+proto = MetroClick.prototype;
+
+//-------------------------------------------------------------------------------------------
+//  PROCESS
+//-------------------------------------------------------------------------------------------
+
+proto.process = function(input) {
+
+    // count //
+    this.i++;
+    if (this.i>=this.duration) {
+        this.kill();
+    }
+
+    // envelope //
+    this.a = common.ADSREnvelope(this.i,this.duration,this.adsr);
+
+    var signal = this.voice.process(this.pitch);
+
+    return [
+        input[0] + signal,
+        input[1] + signal
+    ];
+};
+
+
+//-------------------------------------------------------------------------------------------
+//  KILL
+//-------------------------------------------------------------------------------------------
+
+proto.kill = function() {
+
 };
 
 
